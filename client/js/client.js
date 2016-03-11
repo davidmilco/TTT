c616bd79aad6d26830069c79ad8832c66246a189
David Milco <davidmilco@gmail.com>
2016-03-11 13:03:15 -0600

    Adding Body Parser and a BoardState function
    Adding Body Parser so that Node can better handle the board state that
    will be passed into it.
    
    Added a Boardstate function that updates the state of the board at every
    play so it can be sent after every play to the winConditions function.

--- client/js/client.js
+++ client/js/client.js
@@ -1,20 +1,46 @@
 $(document).ready(function () {
-    var turnCount = 1;
-    var boardState = [];
+    var turnCount = 0;
+    var boardState = [['empty', 'empty', 'empty'], ['empty', 'empty', 'empty'], ['empty', 'empty', 'empty']];
     $('.gameSquare').one('click', function (event) {
+        turnCount++;
         if (turnCount % 2) {
-            $(this).children().text('X').addClass("letterInput");
-            turnCount++;
-            winConditions();
-
+            $(this).children().text('X').addClass("letterInput").addClass('x');
+            updateBoard();
+            winConditions(boardState);
+            
         } else {
-            $(this).children().text('O').addClass("letterInput");
-            turnCount++;
-            winConditions();
+            $(this).children().text('O').addClass("letterInput").addClass('o');
+            updateBoard();
+            winConditions(boardState);
         }
     });
 
     var winConditions = function (boardState) {
         console.log("win conditions function run");
+        console.log('Board State going in:', boardState);
+        $.ajax({
+            url: '/api/winConditions',
+            type: 'POST',
+            data: JSON.stringify(boardState),
+            contentType: 'application/json',
+            success: function (data) {
+                console.log('success with return data of:', data);
+            },
+            error: function (data) {
+                console.log('error with return data of:', data);
+            }
+        });
+    }
+
+    var updateBoard = function () {
+        boardState[0][0] = $('.zeroZero').children().html();
+        boardState[0][1] = $('.zeroOne').children().html();
+        boardState[0][2] = $('.zeroTwo').children().html();
+        boardState[1][0] = $('.oneZero').children().html();
+        boardState[1][1] = $('.oneOne').children().html();
+        boardState[1][2] = $('.oneTwo').children().html();
+        boardState[2][0] = $('.twoZero').children().html();
+        boardState[2][1] = $('.twoOne').children().html();
+        boardState[2][2] = $('.twoTwo').children().html();      
     }
 });
--- server/server.js
+++ server/server.js
@@ -1,13 +1,20 @@
 var express = require('express');
 var http = require("http");
 var path = require('path');
+var parser = require('body-parser');
 var app = express();
 
 app.use('/client', express.static(__dirname + '/../client'));
+app.use(parser.json());
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/../client/index.html'));
 });
+
+app.post('/api/winConditions', function (req, res) {
+    console.log(req.body);
+    res.status(200).send('dummy response');
+})
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
